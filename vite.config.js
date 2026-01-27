da7fc3ce2ce4aa7a32ded844a179610021ad6a89
Dom Phongsakon <dompsk2547@gmail.com>
2026-01-27 19:06:03 +0700

    no message

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Port_Psk/',   // 🔴 สำคัญมาก
 })

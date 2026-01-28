361d29f79d542a09cd1200309478d6bf594f40ae
Dom Phongsakon <dompsk2547@gmail.com>
2026-01-28 16:34:28 +0700

    remove footer and update routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import Navbar from "./components/Navbar/Navbar.jsx";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
 import Project from "./components/Project/Project.jsx";
-import DescProject from "./components/Project/DescProject.jsx"; // 2. นำเข้า DescProject
+import DescProject from "./components/Project/DescProject.jsx"; // 
+import Footer from "./components/footer/Footer.jsx";
 
 const App = () => {
   return (
